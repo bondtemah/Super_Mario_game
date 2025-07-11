d99cdac00b24c9b49df2d243cb74c2a06ad40027
bondtemah <artemichbond@gmail.com>
2025-07-11 23:21:45 +0300

    Пофиксил баг со звуком собирания монет

--- coin.gd
+++ coin.gd
@@ -1,17 +1,10 @@
 extends Sprite2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+var is_collected = false
 
 func _on_interaction_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and not is_collected:
+		is_collected = true
 		$AudioCoin.play()
 		hide()
+		
